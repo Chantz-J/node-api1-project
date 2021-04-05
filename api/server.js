41547ff8b83a49ae029bc36a9c52045b211e7773
Chantz-J <johnson.chantz@gmail.com>
2021-04-05 17:44:50 -0400

    First commit. Just refreshing a bit from the lecture.

--- api/server.js
+++ api/server.js
@@ -1,3 +1,16 @@
 // BUILD YOUR SERVER HERE
+const http = require('http')
+const hostname =  "localhost"
+const port = 3000 
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader("Content-Type", "text/plain")
+    res.end("Hello World from Node\m")
+})
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}: ${port}`)
+})
+
+module.exports = {server}; // EXPORT YOUR SERVER instead of {}
