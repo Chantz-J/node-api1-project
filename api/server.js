fde34ef87cc6529b12e5d60bc6f1816b74b1c44b
Chantz-J <johnson.chantz@gmail.com>
2021-04-05 18:51:22 -0400

    GET request for all users logs to the terminal. But postman is returning 500 error, with an unrelated error message.

--- api/server.js
+++ api/server.js
@@ -1,12 +1,32 @@
 // BUILD YOUR SERVER HERE
 const express = require('express')
+const Users = require('./users/model')
+
+//Server use express modules
 const server = express()
 
+//Middlware of server
 server.use(express.json())
 
+//[GET] Test req :)
 server.get('/', (req, res) => {
     res.send('Hello from Express ^_^')
 })
 
+// [GET] /api/users (R of CRUD, return all users)
+server.get('/api/users', (req, res) => {
+    Users.find()
+    .then(users => {
+        console.log(users)
+        res.status(200).json(dogs)
+    })
+    .catch(err => {
+        res.status(500).json({message: err.message})
+    })
+})
+   
 
-module.exports = {server} // EXPORT YOUR SERVER instead of {}
+
+
+
+module.exports = server // EXPORT YOUR SERVER instead of {}
